79249ebab9c5f79431c016697f2dcb33852e6779
Jaroslaw Wroblewski <kontakt@d3sign.pl>
2018-06-11 13:41:41 +0200

    add js file to project

--- src/js/chipsjs.js
+++ src/js/chipsjs.js
@@ -1,1 +1,29 @@
-console.log('Ready!');
+
+/*
+    Author: Jaroslaw Wroblewski
+    Repo:   https://github.com/jaroslawwroblewski/chipsjs
+ */
+
+(function (factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
+    } else {
+        // browser globals
+        factory(jQuery);
+    }
+
+}(function ($) {
+
+    'use strict';
+    var chipsjs = (function() {
+
+        console.log('ready');
+
+    })();
+
+}));
