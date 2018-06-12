216a74d3c5af01663da929a0ea8cd65f09eb5a8d
Jaroslaw Wroblewski <kontakt@d3sign.pl>
2018-06-12 11:02:20 +0200

    add handler and favicon

--- src/js/chipsjs.js
+++ src/js/chipsjs.js
@@ -20,10 +20,88 @@
 }(function ($) {
 
     'use strict';
-    var chipsjs = (function() {
+    var ChipsJS = (function() {
 
-        console.log('ready');
+        function ChipsJS() {
+
+            var self = this;
+
+
+            var state = {
+              currentLink: ''
+            };
+
+            var  redirect = function(url) {
+                window.location.href = url;
+            };
+
+            var showSelectedMenu = function ($element) {
+                console.log($element);
+            };
+
+
+            var buildMenu = function (elements) {
+              $('body').append('<div class="chipsjs__countainer">Hello World</div>')
+            };
+
+
+            var attachClickHandler = function($elements) {
+                $elements.on('click.chipsjs', function(event) {
+                    event.preventDefault();
+
+                    state.currentLink = $(this);
+
+                    var $selectedMenu = $(this).siblings(self.options.ulNestedClass);
+                    $selectedMenu.length > 0 ? showSelectedMenu($selectedMenu) : redirect($(this).attr('data-href'));
+
+                });
+            };
+
+
+            this.run = function() {
+
+                buildMenu();
+                attachClickHandler(self.$elem.find('[data-href]'));
+            }
+
+
+        }
+
+        ChipsJS.prototype.init = function (options, elem) {
+            this.$elem = $(elem);
+            this.options = $.extend({}, $.fn.chipsjs.default, options);
+
+            return this.options;
+        };
+
+        return ChipsJS;
 
     })();
+
+    $.fn.chipsjs = function (method, options) {
+        return this.each(function () {
+            var plugin = new ChipsJS();
+
+            if (plugin.hasOwnProperty(method)) {
+                plugin.init(options, this);
+                return plugin.run();
+            } else if (typeof method === 'object' || !method) {
+                options = method;
+                plugin.init(options, this);
+                return plugin.run();
+            } else {
+                $.error('Method ' + method + ' does not exist in ChipsJS');
+            }
+        });
+    };
+
+    $.fn.chipsjs.default = {
+        dataType: 'html',
+        ulClass: '.chipsjs__ul',
+        liClass: '.chipsjs__li',
+        ulNestedClass: '.chipsjs__ul-nested'
+    };
+
+    $.fn.chipsjs.ChipsJS = ChipsJS;
 
 }));
