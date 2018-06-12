a854f86a854b816dc7f9708e2eae82d19a5c3ced
Jaroslaw Wroblewski <kontakt@d3sign.pl>
2018-06-12 13:54:25 +0200

    add focus acc support

--- src/js/chipsjs.js
+++ src/js/chipsjs.js
@@ -36,12 +36,22 @@
             };
 
             var showSelectedMenu = function ($element) {
-                console.log($element);
+                $('body').find(self.options.menu + ' ' + self.options.container).html($element.show());
+                $('body').find(self.options.wrapper).show();
+                $('body').find(self.options.close).focus();
+
+            };
+
+            var hideSelectedMenu = function () {
+                $('body').find(self.options.menu + ' ' + self.options.container).html('');
+                $('body').find(self.options.wrapper).hide();
+                $(state.currentLink).focus();
             };
 
 
-            var buildMenu = function (elements) {
-              $('body').append('<div class="chipsjs__countainer">Hello World</div>')
+            var buildMenu = function () {
+                $('body').append(self.options.appendHtml);
+                attachClosekHandler($('body').find(self.options.close));
             };
 
 
@@ -51,20 +61,23 @@
 
                     state.currentLink = $(this);
 
-                    var $selectedMenu = $(this).siblings(self.options.ulNestedClass);
+                    var $selectedMenu = $(this).siblings(self.options.ulNested);
                     $selectedMenu.length > 0 ? showSelectedMenu($selectedMenu) : redirect($(this).attr('data-href'));
 
                 });
             };
 
+            var attachClosekHandler = function($elements) {
+                $elements.on('click.chipsjs', function(event) {
+                    hideSelectedMenu();
+                });
+            };
+
 
             this.run = function() {
-
                 buildMenu();
                 attachClickHandler(self.$elem.find('[data-href]'));
             }
-
-
         }
 
         ChipsJS.prototype.init = function (options, elem) {
@@ -97,9 +110,14 @@
 
     $.fn.chipsjs.default = {
         dataType: 'html',
-        ulClass: '.chipsjs__ul',
-        liClass: '.chipsjs__li',
-        ulNestedClass: '.chipsjs__ul-nested'
+        ul: '.chipsjs__ul',
+        li: '.chipsjs__li',
+        ulNested: '.chipsjs__ul-nested',
+        wrapper: '.chipsjs__wrapper',
+        close: '.chipsjs__close',
+        menu: '.chipsjs__menu',
+        container: '.container',
+        appendHtml: '<div class="chipsjs__wrapper"><button class="chipsjs__close"><span class="u-acc-hide">Close</span></button><div class="chipsjs__menu"><div class="container"></div></div></div>'
     };
 
     $.fn.chipsjs.ChipsJS = ChipsJS;
